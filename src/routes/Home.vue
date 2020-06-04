ef49f607e815807a044d7c2d08d7d99933c69f27
Anoesj <anoesjsadraee@gmail.com>
2020-06-04 13:26:22 +0200

    Displaying base path

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <img alt="Vue logo" :src="logoPath"/>
   <HelloWorld msg="Vue 3.0 + Vite + GitHub Actions experiment"/>
-  <p>Environment: {{ env }}</p>
-  <p>Custom env variable test: {{ customEnvVarTest }}</p>
+  <br/>
+  <p>Environment: <strong>{{ env }}</strong></p>
+  <p>Base path: <strong>{{ basePath }}</strong></p>
+  <p>Custom env variable test: <strong>{{ customEnvVarTest }}</strong></p>
 </template>
 
 <script>
@@ -16,6 +18,7 @@
         logoPath,
         env: process.env.NODE_ENV,
         customEnvVarTest: process.env.VUE_APP_TEST,
+        basePath: process.env.BASE_URL,
       };
     },
     components: {
