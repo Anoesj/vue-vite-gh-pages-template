5a04feaabc886a96dc68a1d301fc09e56bdaf4fa
Anoesj <anoesjsadraee@gmail.com>
2020-06-06 15:11:37 +0200

    Trying fix for GH Pages SPA hack

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const baseURL = process.env.BASE_URL;
-console.log(`BASE_URL: ${baseURL}\nRouter base: ${location.pathname}`)
+console.log(`BASE_URL: ${baseURL}\nRouter base: ${location.pathname}`);
+
 const routerHistory = createWebHistory(baseURL);
 
 const router = createRouter({
@@ -21,6 +22,13 @@ const router = createRouter({
     },
   ],
 });
+
+const { redirect } = sessionStorage;
+delete sessionStorage.redirect;
+if (redirect && redirect !== location.href) {
+  console.log('Going to redirect to ' + redirect);
+  router.push(redirect);
+}
 
 router.beforeEach((to, from, next) => {
   console.log(`%c${from ? from.path + ' ' : ''}→ ${to.path}`, 'background-color: #dee5ec; color: LightSlateGrey; padding: 2px 6px; border-radius: 3px;');
