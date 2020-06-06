c8c9c34547ae7d1874ccc70cd3cd26a1191f2803
Anoesj <anoesjsadraee@gmail.com>
2020-06-06 15:31:43 +0200

    Trying another fix

--- src/router.js
+++ src/router.js
@@ -32,7 +32,12 @@ if (redirect && redirect !== location.pathname) {
 }
 
 router.beforeEach((to, from, next) => {
-  console.log(`%c${from ? from.path + ' ' : ''}→ ${to.path}`, 'background-color: #dee5ec; color: LightSlateGrey; padding: 2px 6px; border-radius: 3px;');
+  if (from.path === to.path) {
+    console.trace(`%c${from ? from.path + ' ' : ''}→ ${to.path}`, 'background-color: #dee5ec; color: LightSlateGrey; padding: 2px 6px; border-radius: 3px;');
+  }
+  else {
+    console.log(`%c${from ? from.path + ' ' : ''}→ ${to.path}`, 'background-color: #dee5ec; color: LightSlateGrey; padding: 2px 6px; border-radius: 3px;');
+  }
   next();
 })
 
